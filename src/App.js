ffb074f02f9f7f7bf6b677c037281ce539f11f63
Sherry Yuan <frostyyshadows@gmail.com>
2019-07-22 19:30:31 -0700

    added stats grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Days from './components/Days.jsx';
+import StatsGrid from './components/StatsGrid.jsx';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Days/>
+      <div className="App">
+        <Days/>
+        <StatsGrid/>
+      </div>
     );
   }
 }
---
+++ src/tileData.js
@@ -0,0 +1,31 @@
+const tileData = [
+  {
+    img: '/static/images/grid-list/breakfast.jpg',
+    title: 'Raisu dinners',
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: '/static/images/grid-list/burgers.jpg',
+    title: 'Countries',
+    author: 'director90',
+  },
+  {
+    img: '/static/images/grid-list/camera.jpg',
+    title: 'Book club books',
+    author: 'Danson67',
+  },
+  {
+    img: '/static/images/grid-list/morning.jpg',
+    title: 'Github repos',
+    author: 'fancycrave1',
+    featured: true,
+  },
+  {
+    img: '/static/images/grid-list/hats.jpg',
+    title: 'Concerts',
+    author: 'Hans',
+  },
+];
+
+export default tileData;
